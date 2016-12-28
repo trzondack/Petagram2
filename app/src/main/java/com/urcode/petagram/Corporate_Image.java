8039a78f122804d40b4d5096507ee88539c2da3f
zondack <zondack_rodriguez@hotmail.com>
2016-12-28 17:50:50 -0600

    imagen corporativa

---
+++ app/src/main/java/com/urcode/petagram/Corporate_Image.java
@@ -0,0 +1,13 @@
+package com.urcode.petagram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Corporate_Image extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_corporate__image);
+    }
+}
